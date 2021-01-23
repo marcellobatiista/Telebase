0f49301f6201bdec2216f6f91791860ea5dffcc7
Marcelo Batista <39688655+marcellobatiista@users.noreply.github.com>
2021-01-23 15:49:11 -0300

    fact

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from pyrogram import Client
-from telebase.base import Base
+from telebase.dados import Dados
 
 app = Client(session, api_id, api_hash)
 
-b = Base(app,'me')
-me = b.consultar('357159852')
-
-print(me.dado)
-
+class Bot:
+  
+  base = None
+  
+  def __init__(self, client):
+    self.base = Dados(client, 'https://')
+    
+  def search(self, info):
+    self.base.buscar(info)
+    
+  def get_data(self):
+    return self.base.dados()
+  
+  def set_value(self, key, new_value):
+    return self.base.editarValor(key, new_value)
+  
+  def add_line(self, key, value):
+    return self.base.adicionarDado(key, value)
+  
+  def remove_data(self, key):
+    return self.base.removerDado(key)
